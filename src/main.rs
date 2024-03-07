42c3907e39e49ff80a0a83ae57e64bf87d5ba07e
AminoCasino <3vqwau76@duck.com>
2024-03-07 01:13:39 -0600

    chapter 2 - observe keypresses

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,18 @@ fn main() {
     let _stdout = enable_raw_mode().unwrap();
     
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{c}");
+        let b = b.unwrap();
+        let c = b as char;
+        // ! NOTE: crossterm's raw mode is different than termion's
+        // ! raw mode. With termion's raw mode, at this point in
+        // ! the tutorial, each key press would be printed - even
+        // ! F2, ESC, Tab, Home, End, Insert, Delete, etc.
+        // ! With crossterm such is not the case.
+        if c.is_control() {
+            println!("{:?}\r", b);
+        } else {
+            println!("{:?} ({})", b, c);
+        }
         if c == 'q' {
             break;
         }
