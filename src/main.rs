68f980a7494f441d8fdf6a33004ed489fee0a1b8
AminoCasino <3vqwau76@duck.com>
2024-03-06 23:01:23 -0600

    chapter 2 - into raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use crossterm::terminal::enable_raw_mode;
 
 fn main() {
+    let _stdout = enable_raw_mode().unwrap();
+    
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
         println!("{c}");
